416605349955d055394b932fc8353d1d6828b9d2
Kineolyan <kineolyan@gmail.com>
2019-02-16 14:57:31 +0100

    Add some unit tests for v1.

--- fs.py
+++ fs.py
@@ -4,11 +4,11 @@ import sys
 class SystemApi:
   def expanduser(self, path):
     return os.path.expanduser(path)
-  
-  def getcwd(self): 
+
+  def getcwd(self):
     return os.getcwd()
 
-  def isdir(self, path): 
+  def isdir(self, path):
     return os.path.isdir(path)
 
   def listdir(self, path):
@@ -22,5 +22,52 @@ class SystemApi:
 
   def symlink(self, src, dst):
     return os.symlink(src, dst)
+
+class TestApi:
+
+  def __init__(self, **kwargs):
+    self.home = kwargs.get('home', os.path.join('/', 'home', 'charlie'))
+    self.cwd = kwargs.get('cwd', os.path.join(self.home, 'working', 'in'))
+    self.clear()
+
+  def clear(self):
+    self.dirs = set()
+    self.entries = {}
+    self.files = set()
+    self.created_dirs = []
+    self.created_links = []
+
+  def expanduser(self, path):
+    if path.startswith('~'):
+      return f"{self.home}{path[1:]}"
+    else:
+      return path
+
+  def getcwd(self):
+    return self.cwd
+
+  def isdir(self, path):
+    return path in self.dirs
+
+  def _set_dir(self, path):
+    self.dirs.add(path)
+
+  def listdir(self, path):
+    return self.entries[path]
+
+  def _set_entries(self, path, entries):
+    self.entries[path] = entries
+
+  def exists(self, path):
+    return path in self.files
+
+  def _set_file(self, path):
+    self.files.add(path)
+
+  def mkdir(self, path):
+    self.created_dirs.add(path)
+
+  def symlink(self, src, dst):
+    self.created_links.add((src, dst))
 
 SYSTEM = SystemApi()
--- v1_test.py
+++ v1_test.py
@@ -1,11 +1,40 @@
 import unittest
 
 import v1
+from fs import TestApi
 
 class V1Test(unittest.TestCase):
 
-  def test_install(self):
-    pass
+  def test_format_with_absolute_path(self):
+    fs = TestApi()
+
+    entry = v1.format_entry('vdir', '/root/some/path:one/dir', fs)
+    self.assertEqual(entry, ('/root/some/path', 'one/dir'))
+
+  def test_format_with_user_home(self):
+    fs = TestApi()
+    entry = v1.format_entry('vdir', '~/my/root:one/dir', fs)
+    self.assertEqual(entry, (f"{fs.home}/my/root", 'one/dir'))
+
+  def test_format_with_relative_path(self):
+    fs = TestApi(cwd = '/a/b/c') # Have
+    entry = v1.format_entry('vdir', 'd:other', fs)
+    self.assertEqual(entry, ('/a/b/c/vdir/d', 'other'))
+
+  def test_format_with_rewinding_path(self):
+    fs = TestApi(cwd = '/a/b/c') # Have
+    entry = v1.format_entry('vdir', '../../and/up/again:other', fs)
+    self.assertEqual(entry, ('/a/b/and/up/again', 'other'))
+
+  def test_format_with_leading_base(self):
+    fs = TestApi(cwd = '/a/b/c')
+    entry = v1.format_entry('to/vdir', '../path:target', fs)
+    self.assertEqual(entry, ('/a/b/c/to/path', 'target'))
+
+  def test_format_with_rewinding_base(self):
+    fs = TestApi(cwd = '/a/b/c')
+    entry = v1.format_entry('../vdir', '../path:target', fs)
+    self.assertEqual(entry, ('/a/b/path', 'target'))
 
 if __name__ == '__main__':
   unittest.main()
